f5292fa1277067f6c715dc519dc17c9a451e12c1
flondono <flondono89@gmail.com>
2021-06-13 18:21:37 -0500

    primer commit

---
+++ src/test/java/com/flondono/springbootbasico/SpringBootBasicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flondono.springbootbasico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
